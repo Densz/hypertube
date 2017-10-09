0576324536ad085c78685158c9ee6fd0e96f58e7
Densz <zheng.denis@gmail.com>
2017-10-09 19:29:09 +0200

    commented communications between server and front

--- client/src/index.js
+++ client/src/index.js
@@ -3,35 +3,6 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
-
-function search() {
-    return fetch("/api/index", {
-        accept: "application/json"
-    })
-    .then(checkStatus)
-    .then(parseJSON)
-}
-
-function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response;
-    }
-    const error = new Error(`HTTP Error ${response.statusText}`);
-    error.status = response.statusText;
-    error.response = response;
-    console.log(error); // eslint-disable-line no-console
-    throw error;
-}
-
-function parseJSON(response) {
-  return response.json();
-}
-
-const Client = search()
-.then((Client) => {
-    console.log(Client);
-});
-
 class Index extends Component {
     render() {
         return (
@@ -43,3 +14,35 @@ class Index extends Component {
 ReactDOM.render(<Index />, document.getElementById('root'));
 
 registerServiceWorker();
+
+/*
+** Do not delete, comments below!
+*/
+
+// function search() {
+//     return fetch("/api/index", {
+//         accept: "application/json"
+//     })
+//     .then(checkStatus)
+//     .then(parseJSON)
+// }
+
+// function checkStatus(response) {
+//     if (response.status >= 200 && response.status < 300) {
+//         return response;
+//     }
+//     const error = new Error(`HTTP Error ${response.statusText}`);
+//     error.status = response.statusText;
+//     error.response = response;
+//     console.log(error); // eslint-disable-line no-console
+//     throw error;
+// }
+
+// function parseJSON(response) {
+//   return response.json();
+// }
+
+// const Client = search()
+// .then((Client) => {
+//     console.log(Client);
+// });
