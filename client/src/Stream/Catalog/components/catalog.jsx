5cf80547367b5c496a59d4b7fdcc91de6181f124
Denis Zheng <dzheng@Deniss-MacBook-Pro.local>
2017-10-17 17:34:57 +0200

    esLint

--- client/src/Stream/Catalog/components/catalog.jsx
+++ client/src/Stream/Catalog/components/catalog.jsx
@@ -3,26 +3,26 @@ import Thumbnail from './thumbnail';
 import '../css/catalog.css';
 
 const catalogtest = {
-    img: "https://yts.ag/assets/images/movies/war_for_the_planet_of_the_apes_2017/medium-cover.jpg",
-    title: "Planet des singes",
-    rating: "3",
-}
+	img: 'https://yts.ag/assets/images/movies/war_for_the_planet_of_the_apes_2017/medium-cover.jpg',
+	title: 'Planet des singes',
+	rating: '3',
+};
 
 class Catalog extends Component {
-    componentDidMount() {
-        let bodyStyle = document.body.style;
+	componentDidMount() {
+		const bodyStyle = document.body.style;
 
-        bodyStyle.backgroundColor = '#20232a';
-    }
-    
-    render() {
-        return (
-            <div className="row ">
-                <Thumbnail infos={catalogtest}/>
-                <Thumbnail infos={catalogtest}/>
-            </div>                
-        )
-    }
+		bodyStyle.backgroundColor = '#20232a';
+	}
+
+	render() {
+		return (
+			<div className="row">
+				<Thumbnail infos={catalogtest} />
+				<Thumbnail infos={catalogtest} />
+			</div>
+        );
+	}
 }
 
 export default Catalog;
--- client/src/Stream/Catalog/components/thumbnail.jsx
+++ client/src/Stream/Catalog/components/thumbnail.jsx
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
 
 class Thumbnail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            infoToggled: false,
-            divClass: 'movie-container-normal'
-        }
-        this.showInfo = this.showInfo.bind(this);
-    }
+		constructor(props) {
+			super(props);
+			this.state = {
+				infoToggled: false,
+				divClass: 'movie-container-normal',
+		};
+			this.showInfo = this.showInfo.bind(this);
+	}
 
-    showInfo() {
-        this.setState((prevState) => ({
-            infoToggled: !prevState.infoToggled,
-        }))
-        if (this.state.divClass === 'movie-container-normal')
-            this.setState({ divClass: 'movie-container-bigger' });
-        else
-            this.setState({ divClass: 'movie-container-normal' });
-    }
+		showInfo() {
+			this.setState((prevState) => ({
+				infoToggled: !prevState.infoToggled,
+		}));
+			if (this.state.divClass === 'movie-container-normal')
+				this.setState({ divClass: 'movie-container-bigger' });
+			else
+			this.setState({ divClass: 'movie-container-normal' });
+	}
 
-    render() {
-        return (
-            <div className={this.state.divClass} onMouseEnter={this.showInfo} onMouseLeave={this.showInfo}>
-                <div className="movie-details">
-                    <img alt={this.props.infos.title} src={this.props.infos.img} className="img-movie"/>
-                    { this.state.infoToggled &&
-                        <div className="about-movie" >
-                            <p className="movie-title">{this.props.infos.title}</p>
-                            <p className="movie-rating">{this.props.infos.rating}</p>
-                        </div>
-                    }
-                </div>
-            </div>
-        );
-    }
+		render() {
+			return (
+			<div className={this.state.divClass} onMouseEnter={this.showInfo} onMouseLeave={this.showInfo}>
+				<div className="movie-details">
+					<img alt={this.props.infos.title} src={this.props.infos.img} className="img-movie" />
+					{this.state.infoToggled &&
+						<div className="about-movie" >
+							<p className="movie-title">{this.props.infos.title}</p>
+							<p className="movie-rating">{this.props.infos.rating}</p>
+						</div>
+					}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Thumbnail;
