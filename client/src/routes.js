ffbb34c7eca9a809f5b122e3ab3fce03f69f0a98
Aeners <arlecomt@student.42.fr>
2017-10-19 20:29:10 +0200

    Redirect si logged, join state and passeport

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import SignIn from "./Form/SignIn/components/SignIn";
 import SignUp from "./Form/SignUp/components/SignUp";
 import Layout from "./General/templates/components/Layout";
 import Catalog from "./Stream/Catalog/components/catalog";
 
 class Routes extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLogged: true
+		}
+	}
+
     render() {
         return (
             <Layout>
                 <Router>
                     <div>
                         <Switch>
-                            <Route exact path="/" component={SignIn} />
+                            <Route exact path="/" render={() => (
+								this.state.isLogged ? (<Redirect to="/catalog"><Catalog /></Redirect>) : <SignIn />)} />
                             <Route path="/signUp" component={SignUp} />
                             <Route path="/catalog" component={Catalog} />
                         </Switch>
