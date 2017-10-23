1bb053ec89b064ced0fc3ce7debc082ade1aca70
Denis Zheng <zheng.denis@gmail.com>
2017-10-23 12:16:44 +0200

    catalog css need to do infinite scroll

--- client/src/Stream/Catalog/components/thumbnail.jsx
+++ client/src/Stream/Catalog/components/thumbnail.jsx
@@ -19,10 +19,13 @@ class Thumbnail extends Component {
 		return (
 			<div className="movie-details">
 				<img className="img-movie" alt={this.props.infos.title_english} src={this.props.infos.medium_cover_image} onMouseEnter={this.showInfo} />
-				{this.state.infoToggled &&
+				{ this.state.infoToggled &&
 				<div className="about-movie" onMouseLeave={this.showInfo}>
-					<p className="movie-text">{this.props.infos.title}</p>
-					<p className="movie-text">{this.props.infos.rating}</p>
+					<p className="movie-title">{this.props.infos.title}</p>
+					<p className="movie-year">({this.props.infos.year})</p>
+					<br/><br/>
+					<p className="movie-rating">{this.props.infos.rating}</p>
+					<span className="imdb">IMDB</span>
 				</div>
 				}
 			</div>
