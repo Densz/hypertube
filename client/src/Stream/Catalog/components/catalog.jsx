dd81e6a0fffee27e23489316f33a123964e40001
Denis Zheng <zheng.denis@gmail.com>
2017-10-23 14:47:04 +0200

    infinite scroll assez bizarre

--- client/src/Stream/Catalog/components/catalog.jsx
+++ client/src/Stream/Catalog/components/catalog.jsx
@@ -2,35 +2,58 @@ import React, { Component } from 'react';
 import Thumbnail from './thumbnail';
 import callApi from '../../../ApiCaller/apiCaller';
 import '../css/catalog.css';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Catalog extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			catalog: {},
-			pages: 0
+			catalog: [],
+			pages: 0,
 		}
 	}
 
 	componentDidMount() {
 		const bodyStyle = document.body.style;
 		bodyStyle.backgroundColor = '#20232a';
-		callApi('/api/catalog/')
+		// callApi('/api/catalog/')
+		// .then((catalogMovies) => {
+		// 	this.setState({
+		// 		catalog: catalogMovies.data.movies,
+		// 		pages: Math.floor(catalogMovies.data.movie_count / 16),
+		// 		currentPage: 1
+		// 	})
+		// })
+	}
+
+	callMoreMovies() {
+		callApi('/api/catalog/', 'post', { pages: this.state.pages })
 		.then((catalogMovies) => {
-			this.setState({
+			//let catalogArray = this.state.catalog;
+			this.setState((prevState) => ({
 				catalog: catalogMovies.data.movies,
-				pages: Math.floor(catalogMovies.data.movie_count / 16)
-			})
+				pages: prevState.pages + 1
+			}))
 		})
 	}
 
 	render() {
-		console.log(this.state);
+		let items = [];
+		this.state.catalog.map((movieData, index) => {
+			items.push(<Thumbnail key={index} infos={movieData} />);
+			return items;
+		})
+
 		return (
 			<div className="row">
-				{ this.state.catalog.length > 0 && this.state.catalog.map((movieData, index) => {
-					return (<Thumbnail key={index} infos={movieData} />)
-				}) }
+				<InfiniteScroll
+					pageStart={0}
+					loadMore={this.callMoreMovies.bind(this)}
+					hasMore={true}
+					loader={<div className="loader">Loading ...</div>}
+				>
+					{items}
+				</InfiniteScroll>
 			</div>
 		);
 	}
--- server/routes/catalog.js
+++ server/routes/catalog.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 import request from 'request';
 
-
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-	request('https://yts.ag/api/v2/list_movies.jsonp?limit=16&page=1&sort_by=like_count', function(error, response, body){
+router.post('/', (req, res, next) => {
+	console.log("Npmbres de la pages:");
+	console.log(req.body.pages);
+	request('https://yts.ag/api/v2/list_movies.jsonp?limit=16&page=' + req.body.pages + '&sort_by=like_count', function(error, response, body){
 		let infos = JSON.parse(body);
 		res.json(infos);
 	})
