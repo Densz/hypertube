99dbe27a52be9e783b51b040c856c8e261b47484
Denis Zheng <zheng.denis@gmail.com>
2017-10-23 15:08:14 +0200

    console log removed

--- server/routes/catalog.js
+++ server/routes/catalog.js
@@ -4,8 +4,6 @@ import request from 'request';
 
 /* GET users listing. */
 router.post('/', (req, res, next) => {
-	console.log("Npmbres de la pages:");
-	console.log(req.body.pages);
 	request('https://yts.ag/api/v2/list_movies.jsonp?limit=16&page=' + req.body.pages + '&sort_by=like_count', function(error, response, body){
 		let infos = JSON.parse(body);
 		res.json(infos);
