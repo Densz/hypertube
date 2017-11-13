d04b3b2f0739459e250d217868ec65ca4a20fb98
Denis Zheng <zheng.denis@gmail.com>
2017-11-13 14:38:17 +0100

    catalog fixed

--- client/src/Stream/Catalog/components/catalog.jsx
+++ client/src/Stream/Catalog/components/catalog.jsx
@@ -12,6 +12,7 @@ class Catalog extends Component {
 			catalog: [],
 			pages: 1,
 			searchField: '',
+			hasMore: true,
 			filterBy: 'like_count',
 		}
 		this.updateSearchInput = this.updateSearchInput.bind(this);
@@ -23,24 +24,38 @@ class Catalog extends Component {
 	}
 
 	callMoreMovies() {
+		this.setState((prevState) => ({
+			catalog: prevState.catalog,
+			pages: prevState.pages,
+			searchField: prevState.searchField,
+			filterBy: prevState.filterBy,
+			hasMore: false
+		}))
 		callApi('/api/catalog/', 'post', { pages: this.state.pages, filterBy: this.state.filterBy, searchField: this.state.searchField })
 		.then((catalogMovies) => {
 			let catalogArray = this.state.catalog;
-			if (catalogMovies.data.movies) {
-				catalogMovies.data.movies.length > 0 &&
-					catalogMovies.data.movies.map(
+			if (catalogMovies.data.movies && catalogMovies.data.movies.length > 0) {
+				catalogMovies.data.movies.map(
 					(movieData) => {
 						catalogArray.push(movieData);
-						return undefined;
 					}
 				)
+				this.setState((prevState) => ({
+					catalog: catalogArray,
+					pages: prevState.pages + 1,
+					searchField: prevState.searchField,
+					filterBy: prevState.filterBy,
+					hasMore: catalogMovies.data.movies.length < 16 ? false : true
+				}))
+			} else {
+				this.setState((prevState) => ({
+					catalog: [],
+					pages: 1,
+					searchField: prevState.searchField,
+					filterBy: prevState.filterBy,
+					hasMore: false
+				}))
 			}
-			this.setState((prevState) => ({
-				catalog: catalogArray,
-				pages: prevState.pages + 1,
-				searchField: prevState.searchField,
-				filterBy: prevState.filterBy,
-			}))
 		})
 	}
 
@@ -50,6 +65,7 @@ class Catalog extends Component {
 			pages: 1,
 			searchField: value,
 			filterBy: prevState.filterBy,
+			hasMore: true
 		}))
 	}
 
@@ -70,7 +86,7 @@ class Catalog extends Component {
 				<div className="row">
 					<InfiniteScroll
 						loadMore={this.callMoreMovies.bind(this)}
-						hasMore= {true}
+						hasMore= {this.state.hasMore}
 					>
 						{items}
 					</InfiniteScroll>
