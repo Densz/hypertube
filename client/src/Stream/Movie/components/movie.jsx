36204187f4b4b3d3bf77df9bf31046bec82280d0
Denis Zheng <zheng.denis@gmail.com>
2017-11-17 18:04:44 +0100

    Movie refactored

--- client/src/Stream/Movie/components/movie.jsx
+++ client/src/Stream/Movie/components/movie.jsx
@@ -7,17 +7,7 @@ class Movie extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			title: '',
-			poster: '',
-			genres: [],
-			budget: '',
-			description: '',
-			release_date: '',
-			vote_average: '',
-			vote_count: '',
-			torrent720: '',
-			torrent1080: '',
-			infoTorrent: {},
+			movieInfos: {}
 		}
 	}
 
@@ -27,24 +17,17 @@ class Movie extends Component {
 	}
 
 	componentWillMount() {
-		this.callInfoMovie();
-		this.callInfoTorrent();
+		this.callInfoMovie()
+		this.callInfoTorrent()
 	}
 
 	callInfoMovie() {
 		callApi('/api/movie/', 'post', {imdb_code: this.props.match.params.imdb})
 		.then((infoMovie) => {
 			console.log(infoMovie);
-			this.setState((prevState) => ({
-				title: infoMovie.original_title,
-				poster: infoMovie.poster_path,
-				genres: infoMovie.genres,
-				budget: infoMovie.budget,
-				description: infoMovie.overview,
-				release_date: infoMovie.release_date,
-				vote_average: infoMovie.vote_average,
-				vote_count: infoMovie.vote_count
-			}))
+			this.setState({
+				movieInfos: infoMovie
+			})
 		})
 	}
 
@@ -52,26 +35,21 @@ class Movie extends Component {
 		callApi('/api/torrent/', 'post', {id: this.props.match.params.id})
 		.then((infoTorrent) => {
 			this.setState({
-				infoTorrent: infoTorrent,
+				infoTorrent: infoTorrent
 			});
-			this.testTest();
 		})
-	}
-
-	testTest() {
-		callApi('/api/downloadTorrent', 'post', {url: this.state.infoTorrent})
 	}
 
 	render() {
 		return(
 			<div className="container">
-				<h1 className="movie-title">{this.state.title}</h1>
+				<h1 className="movie-title">{this.state.movieInfos.original_title}</h1>
 				<div className="MovieInfos">
 					<video width="1125" controls>
-						<source src="http://localhost:3002/" type="video/mp4"/>>
+						<source src="http://localhost:3001/api/downloadTorrent" type="video/mp4"/>>
 					</video>
 				</div>
-				<Moviedescription states={this.state}/>
+				<Moviedescription movieInfos={this.state.movieInfos}/>
 			</div>
 		)
 	}
--- client/src/Stream/Movie/components/moviedescription.jsx
+++ client/src/Stream/Movie/components/moviedescription.jsx
@@ -2,24 +2,31 @@ import React, { Component } from 'react';
 
 class Moviedescription extends Component {
     render () {
-        let genres = [];
-        this.props.states.genres.map((item, index) => {
-            genres.push(item.name + ' ');
-            return undefined
-        })
+		console.log(this.props.movieInfos);
+		let genres = [];
+		if (this.props.movieInfos.genres !== undefined) {
+			this.props.movieInfos.genres.map((item, index) => {
+				genres.push(item.name + ' ');
+				return undefined
+			})
+		}
         
         return (
             <div className="row movie-details-block">
-                <div className="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
-                   <p>Genre : {genres}</p>
-                   <p>Budget : {this.props.states.budget + ' $'}</p>
-                   <p>Overview : {this.props.states.description}</p>
-                   <p>Release : {this.props.states.release_date}</p>
-                   <p>Note : {this.props.states.vote_average} ({this.props.states.vote_count} Vote(s))</p>
-                </div>
-                <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                    <img alt="faut mettre une alt sur toute les images" className="img-fluid img-thumbnail" src={"http://image.tmdb.org/t/p/original" + this.props.states.poster} />
-                </div>
+				{ this.props.movieInfos.genres &&
+					<div>
+						<div className="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+							<p>Genre : {genres}</p>
+							<p>Budget : {this.props.movieInfos.revenue + ' $'}</p>
+							<p>Overview : {this.props.movieInfos.overview}</p>
+							<p>Release : {this.props.movieInfos.release_date}</p>
+							<p>Note : {this.props.movieInfos.vote_average} ({this.props.movieInfos.vote_count} Votes)</p>
+						</div>
+						<div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+							<img alt={this.props.movieInfos.original_title} className="img-fluid img-thumbnail" src={"http://image.tmdb.org/t/p/original" + this.props.movieInfos.poster_path} />
+						</div>
+					</div>
+				}
             </div>
         )
     }
