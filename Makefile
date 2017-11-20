36301b4f90adb5dd79651a49c065e7160285c5ee
me-r1can <nlowe31@gmail.com>
2017-11-20 19:40:43 +0100

    video streaming to front

--- Makefile
+++
@@ -1,9 +0,0 @@
-NAME = hypertube
-
-all: $(NAME)
-
-$(NAME):
-	docker stack deploy -c docker-stack.yml $(NAME)
-
-clean:
-	docker stack rm $(NAME)
--- server/routes/torrent.js
+++ server/routes/torrent.js
@@ -1,27 +1,50 @@
 const express = require('express');
 const router = express.Router();
 import request from 'request';
+const rp = require('request-promise-native');
+import { createReadStream } from 'fs';
 const ts = require('torrent-stream');
 const options = require('../config/torrent');
+
 
 router.post('/', (req, res, next) => {
     console.log(req.body.id);
 	request('https://yts.ag/api/v2/movie_details.json?movie_id=' + req.body.id + '&with_images=true&with_cast=true', function(error, response, body){
 		let infos = JSON.parse(body);
 		res.json(infos);
-	})
+	});
 });
 
 function compareSize(a, b) {
-	if (a.length < b.length)
+	if (a.length > b.length)
 		return (-1);
-	else if (a.length > b.length)
+	else if (a.length < b.length)
 		return (1);
 	return (0);
 };
 
-router.get('/:hash', (req, res, next) => {
-	let hash = req.params.hash;
+function compareSeeds(a, b) {
+	if (a.seeds > b.seeds)
+		return (-1);
+	else if (a.seeds < b.seeds)
+		return (1);
+	return (0);
+};
+
+router.get('/:id', async (req, res, next) => {
+	let hash = '',
+		torrents = [];
+	
+	let response = await rp('https://yts.ag/api/v2/movie_details.json?movie_id=' + req.params.id);
+	let data = JSON.parse(response);
+
+	torrents = data.data.movie.torrents;
+
+	if (torrents.length < 1)
+		next();
+	torrents.sort(compareSeeds);
+	hash = torrents[0].hash;
+	console.log(hash);
 
 	let torrent = ts(hash, options);
 	torrent.on('ready', function () {
@@ -30,13 +53,13 @@ router.get('/:hash', (req, res, next) => {
 		if (files.length < 1) {next();}
 		files.sort(compareSize);
 
-		let stream = (files[0]).createReadStream();
-		
 		const header = {
 			'Content-Length': files[0].length,
-			'Content-Type': 'video/mp4',
-		}
+			'Content-Type': 'video/mp4'
+		};
 		res.writeHead(200, header);
+		console.log(files[0].name);
+		let stream = (files[0]).createReadStream();
 		stream.pipe(res);
 	});
 });
