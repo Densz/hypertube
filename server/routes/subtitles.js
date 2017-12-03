624d2cedfba71b184951666b37006e6cdf5ae784
Kevin NETH <kneth@e2r12p14.42.fr>
2017-12-03 19:47:05 +0100

    probleme path subtitles

--- server/routes/subtitles.js
+++ server/routes/subtitles.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 const OS = require('opensubtitles-api');
-// const mkdirp = require('mkdirp');
 const srt2vtt = require('srt2vtt');
 const fs = require('fs');
 
@@ -13,71 +12,79 @@ const OpenSubtitles = new OS({
     ssl: true
 });
 
-// router.post('/', (req, res) => {
+router.post('/', (req, res) => {
+    console.log(req.body);
+    console.log("bitebitelolxd");
     OpenSubtitles.api.LogIn('username', 'password', 'en', 'UserAgent')
     .then(() => {
         OpenSubtitles.search({
-            imdbid: 'tt0117731', //req.body.imdbid
+            imdbid: req.body.imdb,
             season: '',
             episode: ''
         }).then(subtitles => {
             console.log(subtitles);
             if (subtitles.en || subtitles.fr) {
                 if (subtitles.en) {
+                    // console.log(subtitles.en.url);
                     request(subtitles.en.url, function (error, response, body) {
-                        const name = /*req.body.imdbid + 'en';*/ 'tt0117731en';
-                        const pathsrt = '../subtitles/' + name + '.srt';
+                        const name = req.body.imdb + 'en';
+                        console.log(name);
+                        const pathsrt = __dirname + '/subtitles/test.srt';
+                        console.log(pathsrt);
                         fs.writeFile(pathsrt, body, (err) => {
-                            if (err)
-                                throw err;
-                            else {
-                                const srtData = fs.readFileSync(pathsrt);
-                                srt2vtt(srtData, (err, vttData) => {
-                                    const pathvtt = '../subtitles/' + name + '.vtt'
-                                    if (err) throw new Error(err);
-                                    fs.writeFileSync(pathvtt, vttData);
-                                    fs.unlink(pathsrt, (err) => {
-                                        if (err) {
-                                            console.log(err)
-                                            res.send(err)
-                                        }
-                                    })
-                                });
-                            }
+                            if (err) console.log(err);
+                            // else {
+                            //     const srtData = fs.readFileSync(pathsrt);
+                            //     srt2vtt(srtData, (err, vttData) => {
+                            //         const pathvtt = '../subtitles/' + name + '.vtt'
+                            //         if (err)
+                            //             throw new Error(err);
+                            //         else {
+                            //             fs.writeFileSync(pathvtt, vttData);
+                            //             fs.unlink(pathsrt, (err) => {
+                            //                 if (err) {
+                            //                     console.log(err)
+                            //                     res.send(err)
+                            //                 }
+                            //             })
+                            //         }
+                            //     });
+                            // }
                         });
                     });
                 }
-                if (subtitles.fr) {
-                    request(subtitles.fr.url, function (error, response, body) {
-                        const name = /*req.body.imdbid + 'en';*/ 'tt0117731fr';
-                        const pathsrt = '../subtitles/' + name + '.srt';
-                        fs.writeFile(pathsrt, body, (err) => {
-                            if (err)
-                                throw err;
-                            else {
-                                const srtData = fs.readFileSync(pathsrt);
-                                srt2vtt(srtData, (err, vttData) => {
-                                    const pathvtt = '../subtitles/' + name + '.vtt'
-                                    if (err) throw new Error(err);
-                                    fs.writeFileSync(pathvtt, vttData);
-                                    fs.unlink(pathsrt, (err) => {
-                                        if (err) {
-                                            console.log(err)
-                                            res.send(err)
-                                        }
-                                    })
-                                });
-                            }
-                        });
-                    });
-                }
+                // if (subtitles.fr) {
+                //     request(subtitles.fr.url, function (error, response, body) {
+                //         const name = req.body.imdb + 'fr';;
+                //         const pathsrt = '../subtitles/' + name + '.srt';
+                //         fs.writeFile(pathsrt, body, (err) => {
+                //             if (err)
+                //                 throw err;
+                //             else {
+                //                 const srtData = fs.readFileSync(pathsrt);
+                //                 srt2vtt(srtData, (err, vttData) => {
+                //                     const pathvtt = '../subtitles/' + name + '.vtt'
+                //                     if (err) throw new Error(err);
+                //                     fs.writeFileSync(pathvtt, vttData);
+                //                     fs.unlink(pathsrt, (err) => {
+                //                         if (err) {
+                //                             console.log(err)
+                //                             res.send(err)
+                //                         }
+                //                     })
+                //                 });
+                //             }
+                //         });
+                //     });
+                // }
+                res.json({ok: 'ok'});
             } 
             else {
                 console.log("undefinedxdlol");
                 return ;
             }
         })
-        });
-// });
+    });
+});
 
-// module.exports = router;
+module.exports = router;
