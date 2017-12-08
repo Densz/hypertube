d65ecb39ca1e98858ad085c4426d0cfbe87cc902
arlecomt <arlecomt@student.42.fr>
2017-12-08 06:12:32 +0100

    Footer - Underlined chosen language

--- client/src/General/templates/components/Footer.jsx
+++ client/src/General/templates/components/Footer.jsx
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import '../css/footer.css';
 
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            underlined: {en: true, fr: false}
+        };
+        this.handleOnClickEvent = this.handleOnClickEvent.bind(this);
+    }
+
+    componentDidMount() {
+        document.querySelector('#en').style.borderBottom = '1px solid #67DAF9';    // Remplacer #en par la langue favorite     
+    }
+
+    handleOnClickEvent(e) {
+        const language = e.target.id
+        switch(language) {
+            case "en": 
+                this.props.changeLngToEn();
+                document.querySelector('#en').style.borderBottom = '1px solid #67DAF9';
+                document.querySelector('#fr').style.borderBottom = '';
+                break;
+            case "fr": 
+                this.props.changeLngToFr();
+                document.querySelector('#fr').style.borderBottom = '1px solid #67DAF9';
+                document.querySelector('#en').style.borderBottom = '';
+                break;
+            default : console.log('This language is not supported');
+        }
+    }
     render() {
         return(
-            // <div className="navbar-bottom">
-            //     <div className="container">
-            //         Languages : <a href="/ok">Français</a> | <a href="/ok">English</a>
-            //     </div>
-            // </div>
             <footer className="footer">
                 <div className="container-fluid">
                     <div className="row">
-						<p className="col- col-sm-6 col-md-6 col-lg-6  col-xl-6 text-footer"> Languages : <span className="text-footer" onClick={this.props.changeLngToFr}>Français</span> | <span className="text-footer" onClick={this.props.changeLngToEn}>English</span> </p>
-                        <p className="col- col-sm-6 col-md-6 col-lg-6  col-xl-6 text-footer text-right">Designed by Agrumbac and Mprevot</p>
+                        <p className="col- col-sm-6 col-md-6 col-lg-6  col-xl-6 text-footer"> Languages : <span className="text-footer text-footer-language" id='en' onClick={this.handleOnClickEvent}>English</span> | <span className="text-footer text-footer-language" id='fr' onClick={this.handleOnClickEvent}>Français</span></p>
                     </div>
                 </div>
             </footer>
