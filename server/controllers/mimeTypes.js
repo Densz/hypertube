b10096940702a914ca4574aabb46bab53523afc6
Denis Zheng <zheng.denis@gmail.com>
2017-12-16 21:55:30 +0100

    types re-added

---
+++ server/controllers/mimeTypes.js
@@ -0,0 +1,25 @@
+const getMimeType = {
+	'.flv': 'video/x-flv',
+	'.f4v': 'video/mp4',
+	'.f4p': 'video/mp4',
+	'.mp4': 'video/mp4',
+	'.mkv': 'video/matroska',
+	'.asf': 'video/x-ms-asf',
+	'.asr': 'video/x-ms-asf',
+	'.asx': 'video/x-ms-asf',
+	'.avi': 'video/x-msvideo',
+	'.mpa': 'video/mpeg',
+	'.mpe': 'video/mpeg',
+	'.mpeg': 'video/mpeg',
+	'.mpg': 'video/mpeg',
+	'.mpv2': 'video/mpeg',
+	'.mov': 'video/quicktime',
+	'.movie': 'video/x-sgi-movie',
+	'.mp2': 'video/mpeg',
+	'.qt': 'video/quicktime',
+	'.webm': 'video/webm',
+	'.ts': 'video/mp2t',
+	'.ogg': 'video/ogg'
+};
+
+module.exports = getMimeType
